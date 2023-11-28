172fd4ef4f03a382f3dad55cf6cd596adeac35e9
Stephen Welch <7754730+StephenWelch@users.noreply.github.com>
2023-11-27 22:15:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@ from setuptools import setup
 setup(name='gym_safety',
       version='0.0.1',
       install_requires=['gym'], #And any other dependencies required
-      py_modules=['gym_safety']
 )
